cb202d8ed98bf0c78bbf3b6e0e84d4b3f2a79e21
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-02-24 16:57:42 -0800

    feat: web pages now load

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  fs.readFile("index.html", "utf-8", (err, data) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  console.log("req.url", req.url);
+  console.log("req.method", req.method);
 
-    if (err) {
-      console.log(err);
-      return;
-    }
-    // res.end goes inside readFile method
-    res.end(data);
-  });
+  if (req.url === "/" && req.method === "GET") {
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // res.end goes inside readFile method
+      res.end(data);
+    });
+  } else if (req.url === "/about" && req.method === "GET") {
+    fs.readFile("about.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // res.end goes inside readFile method
+      res.end(data);
+    });
+  } else if (req.url === "/contact-me" && req.method === "GET") {
+    fs.readFile("contact-me.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // res.end goes inside readFile method
+      res.end(data);
+    });
+  } else {
+    fs.readFile("404.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // res.end goes inside readFile method
+      res.end(data);
+    });
+  }
 });
 
 // Note to self: Remember to put the port in the parentheses, idiot!!!
