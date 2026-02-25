6f87348647b17e71c63034cb670e3098165bc112
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-02-25 13:50:54 -0800

    feat: refactored to use express

--- index.js
+++ index.js
@@ -1,58 +1,30 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
+const app = express();
+// path needed to define path on server (doesn't understand absolute "./" syntax)
+const path = require("path");
 
-const PORT = 8080;
-
-const server = http.createServer((req, res) => {
-  console.log("req.url", req.url);
-  console.log("req.method", req.method);
-
-  if (req.url === "/" && req.method === "GET") {
-    fs.readFile("index.html", "utf-8", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-
-      if (err) {
-        console.log(err);
-        return;
-      }
-      // res.end goes inside readFile method
-      res.end(data);
-    });
-  } else if (req.url === "/about" && req.method === "GET") {
-    fs.readFile("about.html", "utf-8", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.end(data);
-    });
-  } else if (req.url === "/contact-me" && req.method === "GET") {
-    fs.readFile("contact-me.html", "utf-8", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.end(data);
-    });
-  } else {
-    fs.readFile("404.html", "utf-8", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.end(data);
-    });
-  }
+app.get("/", (req, res) => {
+  // need __dirname for absolute path
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Note to self: Remember to put the port in the parentheses, idiot!!!
-server.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`);
+app.get("/about", (req, res) => {
+  // need __dirname for absolute path
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  // need __dirname for absolute path
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`App listening on port ${PORT}`);
 });
